02957564a824588a39d24584e343d656345caf47
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2026-06-04 13:36:48 +0700

    fix: correct variable names and update employee address logging

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,21 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const foodOrder = {
+    orderNumber: "A0234",
+    address: "Bangkok",
+    paymentType: "Cash",
+    totalPrice: 4500,
+    restaurantName: "MK"
+}
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
+console.log(foodOrder);
+
+// result : {
+//   orderNumber: 'A0234',
+//   address: 'Bangkok',
+//   paymentType: 'Credit Card',
+//   totalPrice: 5000,
+//   restaurantName: 'MK'
+// }
